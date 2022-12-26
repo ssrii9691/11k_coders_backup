bd0a941d74a4eff5a6f2361814d8167a41580867
ssrii9691 <mangalisaikumar24@gmail.com>
2022-12-26 10:54:03 +0530

    ok

---
+++ Reactjs/07_SPA_CRUD/crud1/src/App.js
@@ -0,0 +1,27 @@
+
+import "./App.css";
+import Users from "./Component/Users";
+import {BrowserRouter,Route,Routes,} from 'react-router-dom'
+import Layout from "./Component/Layout";
+import EditUser from "./Component/EditUser";
+import Deleteuser from "./Component/Deleteuser";
+import CreateUser from "./Component/CreateUser";
+import Navbar from "./Component/Navbar";
+function App() {
+  return (
+    
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Users/>}/>
+          <Route path="/edit/:id" element={<EditUser/>}/>
+          <Route path="/delete/:id" element={<Deleteuser/>}/>
+          <Route path="/create" element={<CreateUser/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ Reactjs/07_SPA_CRUD/crud1/src/component/User.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const User = ({ usr }) => {
+  const navigate = useNavigate();
+  const handleDelete = (usr) => {
+    console.log(usr);
+
+    navigate('/delete/'+usr.id)
+  };
+  const hanldeEdit = (usr)=>{
+    navigate('/edit/'+usr.id)
+  }
+  return (
+    <tr>
+      <td>{usr.id}</td>
+      <td>{usr.fname}</td>
+      <td>{usr.lname}</td>
+      <td>{usr.email}</td>
+      <td>{usr.password}</td>
+      <td>
+        <button className="btn btn-warning" onClick={()=>{hanldeEdit(usr)}}>Edit</button>
+      </td>
+      <td>
+        <button
+          className="btn btn-danger"
+          onClick={() => {
+            handleDelete(usr);
+          }}
+        >
+          Delete
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default User;
