b6fadd14220ff7a472b017fc10fafb64d5a418f3
ssrii9691 <mangalisaikumar24@gmail.com>
2023-01-06 17:00:55 +0530

    ok

--- Reactjs/13_react_redux_clscompo_crud/src/store/actions/creatorsAction.js
+++
@@ -1,8 +0,0 @@
-import { ADD_CREATORS } from "./actionTypes"
-
-export const addcreatorAction=(creator)=>{
-    return{
-        type:ADD_CREATORS,
-        payload:creator
-    }
-}
--- Reactjs/13_react_redux_clscompo_crud/src/store/index.js
+++
@@ -1,7 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import { creatorsReducer } from './reducers/CreatorReducer'
-export const MainStore=configureStore({
-    reducer:{
-        creators:creatorsReducer
-    }
-})
